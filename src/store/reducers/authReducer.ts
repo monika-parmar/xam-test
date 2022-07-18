391cdf09a0d54295260ee8a79cc3e730c7957394
Monika Parmar <parmarmonica11@gmail.com>
2022-07-19 04:36:09 +1000

    authReducer created and cases added

---
+++ src/store/reducers/authReducer.ts
@@ -0,0 +1,76 @@
+import { UserDetails, users } from "../../assets/data/users_data";
+import { saveAuthUserNameToLocalStorage } from "../../helpers/localstoragehelper";
+import { AUTH_REDUX_CONSTANTS } from "../reduxConstants/authReduxConstants";
+
+interface authReducerProps {
+  branchId: number | undefined;
+  userName: string;
+  password: string;
+  error: string | undefined;
+  isAuthorised: boolean;
+}
+
+const initialAuthReducer: authReducerProps = {
+  branchId: undefined,
+  userName: "",
+  password: "",
+  error: undefined,
+  isAuthorised: false
+};
+
+export const authReducer = (
+  state = initialAuthReducer,
+  action: { type: string; data: Record<string, any> }
+) => {
+  switch (action?.type) {
+    case AUTH_REDUX_CONSTANTS.ON_CHANGE_AUTH_INPUT_VALUE:
+      const { name, value } = action?.data;
+      return {
+        ...state,
+        [name]: value,
+      };
+
+      case AUTH_REDUX_CONSTANTS.CHANGE_AUTH_STATUS:       
+        return {
+            ...state,
+            isAuthorised: action?.data,
+          };
+
+    case AUTH_REDUX_CONSTANTS.CHECK_CREDENTIALS: {
+      const { branchId, userName, password } = action?.data;
+      
+      const existingUser: UserDetails | undefined = users.find(
+        (user) => user.branchId === parseInt(branchId)
+      );
+      
+      let errorMessage = undefined;
+      let authStatus = false;
+      if (existingUser) {
+        if (
+          userName === existingUser.userName &&
+          password === existingUser.password
+        ) {
+            saveAuthUserNameToLocalStorage(userName);
+            authStatus = true;
+            errorMessage = undefined;
+        } else if(userName !== existingUser.userName && password !== existingUser.password) {
+            errorMessage = "User name and password are incorrect";
+         } else if(userName !== existingUser.userName) {
+            errorMessage = "User name is incorrect";
+         } else if(password !== existingUser.password) {
+                errorMessage = "Password is incorrect";
+            }
+        } else {
+            errorMessage = "No user with provided branch id exist"
+        }
+        return {
+            ...state,
+            error: errorMessage,
+            isAuthorised: authStatus
+        }
+      }
+
+    default:
+      return state;
+  }
+};
