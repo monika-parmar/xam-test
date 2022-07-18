828907cac6039ef99fe3d86da9f9488fdc6663f7
Monika Parmar <parmarmonica11@gmail.com>
2022-07-19 04:32:44 +1000

    app component changes done - login and dashboard components added

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { Provider } from 'react-redux';
+import './App.scss';
+import { getAuthUserNameFromLocalStorage } from './helpers/localstoragehelper';
+import { useAppDispatch, useAppSelector } from './hooks/hooks';
+import Login from './pages/auth/login/Login';
+import Dashboard from './pages/dashboard/Dashboard';
+import { loadExistingUsersAction } from './store/actions/userManagementActions';
+import { store } from './store/redux/store';
+import { AUTH_REDUX_CONSTANTS } from './store/reduxConstants/authReduxConstants';
+
+const App = () => {
+  const dispatch = useAppDispatch();
+  const { isAuthorised } = useAppSelector(
+    ({ authReducer }: Record<string, any>) => authReducer ?? {}
+  );
+
+  useEffect(() => {
+    dispatch(loadExistingUsersAction());
+  
+      const cb = () => {
+          dispatch({
+            type: AUTH_REDUX_CONSTANTS.CHANGE_AUTH_STATUS,
+            status: getAuthUserNameFromLocalStorage() ? true :false,
+          });
+      };
+      window.addEventListener('storage', cb);
+  
+      return () => {
+        window.removeEventListener('storage', cb);
+      };
+ 
+  }, [])
+
+  return (
+    <div className='app-container'>
+    {!isAuthorised ? <Login /> : <Dashboard />}
+    </div>
+  );
+}
+
+export default App;
