b5b3cca4189ae1785bd88e9c230b1e5c59aa9bff
Monika Parmar <parmarmonica11@gmail.com>
2022-07-19 04:41:25 +1000

    user detail form added with stylesheet and constant file

---
+++ src/constants/userDetailFormConstant.ts
@@ -0,0 +1,30 @@
+export const userDetailFormConstant = [
+    {
+    name: "branchId",
+    placeholder: "Branch Id",
+},
+{
+    name: "userName",
+    placeholder: "User Name",
+},
+{
+    name: "firstName",
+    placeholder: "First Name",
+},
+{
+    name: "middleName",
+    placeholder: "Middle Name",
+},
+{
+    name: "lastName",
+    placeholder: "Last Name",
+},
+{
+    name: "position",
+    placeholder: "Position",
+},
+{
+    name: "password",
+    placeholder: "Password",
+},
+]
---
+++ src/pages/dashboard/userDetailForm/UserDetailForm.tsx
@@ -0,0 +1,95 @@
+import { FC, useState } from "react";
+import { UserType } from "../../../assets/data/users_data";
+import Button from "../../../components/button/Button";
+import Input from "../../../components/input/Input";
+import { userDetailFormConstant } from "../../../constants/userDetailFormConstant";
+import { useAppDispatch, useAppSelector } from "../../../hooks/hooks";
+import {
+  onAddUserDetailFormAction,
+  onResetUserDetailFormAction,
+  onUserDetailFormInputChangeAction,
+} from "../../../store/actions/userManagementActions";
+
+const UserDetailForm: FC = () => {
+  const dispatch = useAppDispatch();
+  const [isFormSubmitted, setIsFormSubmitted] = useState<boolean>(false);
+  const [userAlreadyExist, setUserAlreadyExist] = useState<boolean>(false);
+  const onUserDetailFormInputChange = (
+    name: string,
+    value: string | number
+  ) => {
+    dispatch(onUserDetailFormInputChangeAction({ name, value }));
+  };
+  const { users, newUserDetails } = useAppSelector(
+    ({ userManagementReducer }: Record<string, any>) =>
+      userManagementReducer ?? {}
+  );
+  const onUserDetailFormReset = () => {
+    dispatch(onResetUserDetailFormAction());
+  };
+
+  const onUserDetailAdd = () => {
+    let isFormValidated = true;
+    setIsFormSubmitted(true);
+    for (const key in newUserDetails) {
+      if (!newUserDetails[key]?.toString()?.trim()?.length) {
+        isFormValidated = false;
+      }
+    }
+    const existingUser = users?.find(
+      (user: UserType) => user.userName === newUserDetails.userName
+    );
+    if (existingUser) {       
+      isFormValidated = false;
+      setUserAlreadyExist(true);
+    } else {
+        setUserAlreadyExist(false);
+    }
+    if (isFormValidated) {
+      setIsFormSubmitted(false);
+      dispatch(onAddUserDetailFormAction(newUserDetails));
+    }
+  };
+
+  return (
+    <div className="dashboard__user-form">
+      {userDetailFormConstant.map((field, index) => (
+        <div>
+          <Input
+            key={index}
+            placeholder={field.placeholder}
+            value={newUserDetails?.[field?.name]}
+            onChange={(e) =>
+              onUserDetailFormInputChange(field.name, e.target.value)
+            }
+          />
+          {isFormSubmitted && !newUserDetails?.[field?.name]?.length
+             && (
+                <div className="user-form-error">
+                  Please enter {field.placeholder.toLocaleLowerCase()}
+                </div>
+              )}
+              {
+                field?.name === "userName" &&
+                userAlreadyExist ? <div className="user-form-error">
+                Username already exists</div> : ''}
+        </div>
+      ))}
+
+      <div className="user-detail-form-button-container">
+        <Button
+          buttonType="secondary"
+          buttonText="RESET"
+          onClick={onUserDetailFormReset}
+        />
+        <Button
+          buttonType="primary"
+          buttonText="ADD"
+          onClick={onUserDetailAdd}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UserDetailForm;
