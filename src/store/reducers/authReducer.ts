4a3ee9c986342c697245f2edaf4d0c2980305201
Monika Parmar <parmarmonica11@gmail.com>
2022-07-19 12:34:14 +1000

    prettier

--- src/store/reducers/authReducer.ts
+++ src/store/reducers/authReducer.ts
@@ -15,7 +15,7 @@ const initialAuthReducer: authReducerProps = {
   userName: "",
   password: "",
   error: undefined,
-  isAuthorised: false
+  isAuthorised: false,
 };
 
 export const authReducer = (
@@ -30,20 +30,27 @@ export const authReducer = (
         [name]: value,
       };
 
-      case AUTH_REDUX_CONSTANTS.CHANGE_AUTH_STATUS: 
-            const {isAuthorised} = action?.data;
-        return {
-            ...state,
-            isAuthorised,
-          };
+    case AUTH_REDUX_CONSTANTS.CHANGE_AUTH_STATUS:
+      const { isAuthorised } = action?.data;
+      return {
+        ...state,
+        isAuthorised,
+      };
+
+    case AUTH_REDUX_CONSTANTS.SET_LOGGED_IN_USER_NAME:
+      const { userName } = action?.data;
+      return {
+        ...state,
+        userName,
+      };
 
     case AUTH_REDUX_CONSTANTS.CHECK_CREDENTIALS: {
       const { branchId, userName, password } = action?.data;
-      
+
       const existingUser: UserDetails | undefined = users.find(
         (user) => user.branchId === parseInt(branchId)
       );
-      
+
       let errorMessage = undefined;
       let authStatus = false;
       if (existingUser) {
@@ -51,25 +58,28 @@ export const authReducer = (
           userName === existingUser.userName &&
           password === existingUser.password
         ) {
-            saveAuthUserNameToLocalStorage(userName);
-            authStatus = true;
-            errorMessage = undefined;
-        } else if(userName !== existingUser.userName && password !== existingUser.password) {
-            errorMessage = "User name and password are incorrect";
-         } else if(userName !== existingUser.userName) {
-            errorMessage = "User name is incorrect";
-         } else if(password !== existingUser.password) {
-                errorMessage = "Password is incorrect";
-            }
-        } else {
-            errorMessage = "No user with provided branch id exist"
+          saveAuthUserNameToLocalStorage(userName);
+          authStatus = true;
+          errorMessage = undefined;
+        } else if (
+          userName !== existingUser.userName &&
+          password !== existingUser.password
+        ) {
+          errorMessage = "User name and password are incorrect";
+        } else if (userName !== existingUser.userName) {
+          errorMessage = "User name is incorrect";
+        } else if (password !== existingUser.password) {
+          errorMessage = "Password is incorrect";
         }
-        return {
-            ...state,
-            error: errorMessage,
-            isAuthorised: authStatus
-        }
+      } else {
+        errorMessage = "No user with provided branch id exist";
       }
+      return {
+        ...state,
+        error: errorMessage,
+        isAuthorised: authStatus,
+      };
+    }
 
     default:
       return state;
