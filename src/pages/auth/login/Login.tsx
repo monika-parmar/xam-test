f13c71ffae0001d3deece460dbc5eaa45d57fbe8
Monika Parmar <parmarmonica11@gmail.com>
2022-07-19 12:33:12 +1000

    data test ids added

--- src/pages/auth/login/Login.tsx
+++ src/pages/auth/login/Login.tsx
@@ -3,9 +3,10 @@ import Button from "../../../components/button/Button";
 import Input from "../../../components/input/Input";
 import { useAppDispatch, useAppSelector } from "../../../hooks/hooks";
 import { onAuthInputValueChangeAction, onLoginClickAction } from "../../../store/actions/authActions";
+import { LOGIN_PAGE_TEST_CONSTANTS } from "./loginTestConstants";
 
 const Login: FC = () => {
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
   const { branchId, userName, password, error } = useAppSelector(
     ({ authReducer }: Record<string, any>) => authReducer ?? {}
   );
@@ -20,29 +21,32 @@ const Login: FC = () => {
 
   return (
     <div className="login-container">
-      <div>
-        <div className="login-title">Login</div>
+      <div data-testid={LOGIN_PAGE_TEST_CONSTANTS.loginForm}>
+        <label data-testid={LOGIN_PAGE_TEST_CONSTANTS.loginLabel} className="login-title">Login</label>
 
         <Input
+         data-testid={LOGIN_PAGE_TEST_CONSTANTS.branchIdInput}
           type="text"
           placeholder="Branch id"
           onChange={(e) => onInputValueChange("branchId", e.target.value)}
           value={branchId}
         />
         <Input
+         data-testid={LOGIN_PAGE_TEST_CONSTANTS.userNameInput}
           type="text"
           placeholder="User name"
           onChange={(e) => onInputValueChange("userName", e.target.value)}
           value={userName}
         />
         <Input
+         data-testid={LOGIN_PAGE_TEST_CONSTANTS.passwordInput}
           type="password"
           placeholder="Password"
           onChange={(e) => onInputValueChange("password", e.target.value)}
           value={password}
         />
 
-        <Button buttonType="primary" buttonText="LOGIN" onClick={onLoginClick} />
+        <Button data-testid={LOGIN_PAGE_TEST_CONSTANTS.loginButton} buttonType="primary" buttonText="LOGIN" onClick={onLoginClick} />
 
         {error && <div className="login-error-message">Error: {error}</div>}
       </div>
---
+++ src/pages/auth/login/loginTestConstants.ts
@@ -0,0 +1,8 @@
+export const LOGIN_PAGE_TEST_CONSTANTS = {
+    loginForm: 'loginForm',
+    loginLabel: 'loginLabel',
+    branchIdInput: 'branchIdInput',
+    userNameInput: 'userNameInput',
+    passwordInput: 'passwordInput',
+    loginButton: 'loginButton',
+}
